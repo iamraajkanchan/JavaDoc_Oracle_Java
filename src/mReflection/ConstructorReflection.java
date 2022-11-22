bb15fcb3a268903476b37a50b046360e70fe0e1e
iamraajkanchan <iamraajkanchan@gmail.com>
2022-11-22 15:43:50 +0530

    ConstructorReflection.java is updated in reflecton package.

--- src/mReflection/ConstructorReflection.java
+++ src/mReflection/ConstructorReflection.java
@@ -1,4 +1,36 @@
 package mReflection;
 
+import java.lang.reflect.Constructor;
+
 public class ConstructorReflection {
+    public ConstructorReflection() {
+    }
+
+    public ConstructorReflection(int i, double d) {
+
+    }
+
+    public static void main(String[] args) {
+        try {
+            Class<?> c = Class.forName("mReflection.ConstructorReflection");
+            Constructor<?>[] classConstructors = c.getDeclaredConstructors();
+            System.out.println("------------------------------------------");
+            for (Constructor<?> cConstructor : classConstructors) {
+                System.out.println("Constructor Name: " + cConstructor.getName());
+                Class<?>[] parameterTypes = cConstructor.getParameterTypes();
+                for (Class<?> parameterType : parameterTypes) {
+                    System.out.println("Parameter Name: " + parameterType.getName());
+                    System.out.println("Parameter Type: " + parameterType.getTypeName());
+                }
+                Class<?>[] exceptionTypes = cConstructor.getExceptionTypes();
+                for (Class<?> exceptionType : exceptionTypes) {
+                    System.out.println("Exception Name: " + exceptionType.getName());
+                    System.out.println("Exception Type: " + exceptionType.getTypeName());
+                }
+                System.out.println("------------------------------------------");
+            }
+        } catch (Throwable e) {
+            System.out.println(e.getMessage());
+        }
+    }
 }
