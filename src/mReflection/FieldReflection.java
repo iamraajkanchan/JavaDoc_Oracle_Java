ac4fe10b3dcdca4639b4044950a86ed50f71a091
iamraajkanchan <iamraajkanchan@gmail.com>
2022-11-22 16:40:27 +0530

    FieldReflection .java is updated in reflecton package.

--- src/mReflection/FieldReflection.java
+++ src/mReflection/FieldReflection.java
@@ -1,4 +1,28 @@
 package mReflection;
 
+import java.lang.reflect.Field;
+import java.lang.reflect.Modifier;
+
 public class FieldReflection {
+    private double d;
+    public static final int i = 36;
+    String s = "testing";
+
+    public static void main(String[] args) {
+        try {
+            Class<?> c = Class.forName("mReflection.FieldReflection");
+            Field[] classFields = c.getFields();
+            System.out.println("---------------------------------------");
+            for (Field classField: classFields) {
+                System.out.println("Field Name: " + classField.getName());
+                System.out.println("Declaring Class: " + classField.getDeclaringClass());
+                System.out.println("Field Type: " + classField.getType());
+                int fieldModifiers = classField.getModifiers();
+                System.out.println("Field Modifier: " + Modifier.toString(fieldModifiers));
+                System.out.println("---------------------------------------");
+            }
+        } catch (Throwable e) {
+            System.out.println(e.getMessage());
+        }
+    }
 }
