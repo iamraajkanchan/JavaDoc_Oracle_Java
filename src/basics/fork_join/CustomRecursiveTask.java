c3920e8e03fe04104b0629ca5b3ddf664b875b05
iamraajkanchan <iamraajkanchan@gmail.com>
2023-01-25 11:42:10 +0530

    CustomRecursiveTask is added in fork_join package

---
+++ src/basics/fork_join/CustomRecursiveTask.java
@@ -0,0 +1,41 @@
+package basics.fork_join;
+
+import org.jetbrains.annotations.NotNull;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Collection;
+import java.util.List;
+import java.util.concurrent.ForkJoinTask;
+import java.util.concurrent.RecursiveTask;
+
+public class CustomRecursiveTask extends RecursiveTask<Integer> {
+    private final int[] arr;
+    private static final int THRESHOLD = 20;
+
+    public CustomRecursiveTask(int[] arr) {
+        this.arr = arr;
+    }
+
+    @Override
+    protected Integer compute() {
+        if (arr.length > THRESHOLD) {
+            return ForkJoinTask.invokeAll(createSubTasks()).stream().mapToInt(ForkJoinTask::join).sum();
+        } else {
+            return processing(arr);
+        }
+    }
+
+    private @NotNull Collection<CustomRecursiveTask> createSubTasks() {
+        List<CustomRecursiveTask> dividedTasks = new ArrayList<>();
+        System.out.println("You are dividing task in " + Thread.currentThread().getName());
+        dividedTasks.add(new CustomRecursiveTask(Arrays.copyOfRange(arr, 0, arr.length / 2)));
+        dividedTasks.add(new CustomRecursiveTask(Arrays.copyOfRange(arr, arr.length / 2, arr.length)));
+        return dividedTasks;
+    }
+
+    private int processing(int[] arr) {
+        return Arrays.stream(arr).filter(a -> a > 10 && a < 27).map(a -> a * 10).sum();
+    }
+
+}
--- src/basics/fork_join/ForkJoinExample.java
+++ src/basics/fork_join/ForkJoinExample.java
@@ -16,5 +16,8 @@ public class ForkJoinExample {
         }
         CustomRecursiveAction recursiveAction = new CustomRecursiveAction(tempText);
         recursiveAction.compute();
+        CustomRecursiveTask recursiveTask = new CustomRecursiveTask(new int[]{1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20});
+        System.out.println("The result is - " + recursiveTask.compute());
+        ;
     }
 }
