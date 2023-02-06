8ddc7084176dd0c85292bbee60c6f0b639622e2a
iamraajkanchan <iamraajkanchan@gmail.com>
2023-02-06 10:49:29 +0530

    main method is modified in StaticDestinationClass in mStatic package

--- src/mStatic/StaticDestinationClass.java
+++ src/mStatic/StaticDestinationClass.java
@@ -4,5 +4,7 @@ public class StaticDestinationClass {
     public static void main(String[] a) {
         System.out.println(StaticSourceClass.myName("Surya Shukla"));
         StaticAlternateSource staticAlternateSource = new StaticAlternateSource();
+        // An object of a class can access a static method, it is not recommended, and it is also not mentioned in the list of recommendations by a lint.
+        System.out.println(staticAlternateSource.getClassName());;
     }
 }
