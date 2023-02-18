680a326484e603aff1218f5469e703a48dd81feb
iamraajkanchan <iamraajkanchan@gmail.com>
2023-02-18 14:22:03 +0530

    SingletonForSingleAccessPointNetwork is added in singleton/offThread/forSingleAccessPoint inside mPatterns

---
+++ src/mPatterns/singleton/offThread/forSingleAccessPoint/SingletonForSingleAccessPointNetwork.java
@@ -0,0 +1,16 @@
+package mPatterns.singleton.offThread.forSingleAccessPoint;
+
+public final class SingletonForSingleAccessPointNetwork {
+    private final static SingletonForSingleAccessPointNetwork singletonNetwork = new SingletonForSingleAccessPointNetwork();
+
+    private SingletonForSingleAccessPointNetwork() {
+    }
+
+    public static SingletonForSingleAccessPointNetwork getInstance() {
+        return singletonNetwork;
+    }
+
+    public void connectNetwork() {
+        System.out.println("Connecting Network...");
+    }
+}
