3827a22717c08f9b65f4c201c054277bc6689d3c
iamraajkanchan <iamraajkanchan@gmail.com>
2023-02-18 15:56:04 +0530

    main method is modified in SingletonForSingeAccessPointNetworkSimulator class

--- src/mPatterns/singleton/offThread/forSingleAccessPoint/SingletonForSingeAccessPointNetworkSimulator.java
+++ src/mPatterns/singleton/offThread/forSingleAccessPoint/SingletonForSingeAccessPointNetworkSimulator.java
@@ -3,6 +3,6 @@ package mPatterns.singleton.offThread.forSingleAccessPoint;
 public class SingletonForSingeAccessPointNetworkSimulator {
     public static void main(String[] args) {
         SingletonForSingleAccessPointPublicNetwork singleton = new SingletonForSingleAccessPointPublicNetwork();
-        singleton.connectNetwork();
+        singleton.connect();
     }
 }
