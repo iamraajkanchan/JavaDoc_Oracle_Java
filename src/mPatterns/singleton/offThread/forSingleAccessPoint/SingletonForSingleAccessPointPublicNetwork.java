72e0c44d1d11a8bf8ddfafa7e0845f06c02c3569
iamraajkanchan <iamraajkanchan@gmail.com>
2023-02-18 15:40:26 +0530

    SingletonForSingleAccessPointPublicNetwork is modified

--- src/mPatterns/singleton/offThread/forSingleAccessPoint/SingletonForSingleAccessPointPublicNetwork.java
+++ src/mPatterns/singleton/offThread/forSingleAccessPoint/SingletonForSingleAccessPointPublicNetwork.java
@@ -1,4 +1,14 @@
 package mPatterns.singleton.offThread.forSingleAccessPoint;
 
 public class SingletonForSingleAccessPointPublicNetwork {
+    SingletonForSingleAccessPointNetwork singletonNetwork;
+
+    SingletonForSingleAccessPointPublicNetwork() {
+        singletonNetwork = SingletonForSingleAccessPointNetwork.getInstance();
+    }
+
+    public void connectNetwork() {
+        singletonNetwork.connectNetwork();
+        System.out.println("Public network connected.");
+    }
 }
