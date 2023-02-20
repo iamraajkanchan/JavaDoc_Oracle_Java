5e9a594df09e0163cfe0411ab2d848c2ea430a64
iamraajkanchan <iamraajkanchan@gmail.com>
2023-02-20 19:02:47 +0530

    DecoratorMocha is added in decorator package inside mPatterns.

---
+++ src/mPatterns/decorator/DecoratorMocha.java
@@ -0,0 +1,20 @@
+package mPatterns.decorator;
+
+public class DecoratorMocha extends CondimentDecorator {
+
+    private final DecoratorBeverage beverage;
+
+    DecoratorMocha(DecoratorBeverage beverage) {
+        this.beverage = beverage;
+    }
+
+    @Override
+    String getDescription() {
+        return beverage.description + ", Mocha";
+    }
+
+    @Override
+    double cost() {
+        return beverage.cost() + 6.50;
+    }
+}
