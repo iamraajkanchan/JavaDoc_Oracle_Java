160d8e380fddfb274b5229757e0f9b95b0bee7b0
iamraajkanchan <iamraajkanchan@gmail.com>
2023-02-22 18:54:24 +0530

    methods and properties are added in FactoryPizza class.

--- src/mPatterns/factory/FactoryPizza.java
+++ src/mPatterns/factory/FactoryPizza.java
@@ -1,5 +1,63 @@
 package mPatterns.factory;
 
-public interface FactoryPizza {
+abstract class FactoryPizza {
+    String name;
+    FactoryCheese cheese;
+    FactoryClams clams;
+    FactoryDough dough;
+    FactorySauce sauce;
+    String[] veggies;
 
+    abstract void prepare();
+
+    public void bake() {
+        System.out.println("Bake for 25 minutes at 350");
+    }
+
+    public void cut() {
+        System.out.println("Cutting the pizza into diagonal sizes");
+    }
+
+    public void box() {
+        System.out.println("Place Pizza into the PizzaStore box");
+    }
+
+    public void setPizzaName(String name) {
+        this.name = name;
+    }
+
+    public String getName() {
+        return this.name;
+    }
+
+    public String toString() {
+        StringBuilder result = new StringBuilder();
+        result.append("--------").append(this.name).append("--------");
+        if (cheese != null) {
+            result.append(cheese);
+            result.append("/n");
+        }
+        if (clams != null) {
+            result.append(clams);
+            result.append("/n");
+        }
+        if (dough != null) {
+            result.append(dough);
+            result.append("/n");
+        }
+        if (sauce != null) {
+            result.append(sauce);
+            result.append("/n");
+        }
+        if (veggies != null) {
+            for (int i = 0; i < veggies.length; i++) {
+                result.append(veggies[i]);
+                if (i < veggies.length - 1) {
+                    result.append(", ");
+                }
+            }
+            result.append("/n");
+        }
+        return result.toString();
+    }
 }
