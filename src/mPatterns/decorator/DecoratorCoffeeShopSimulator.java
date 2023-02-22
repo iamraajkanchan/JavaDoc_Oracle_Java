5b9e5589154170ef21d373b17e61ebf221e9faa7
iamraajkanchan <iamraajkanchan@gmail.com>
2023-02-22 11:03:57 +0530

    myBeverage instance is reordered in DecoratorCoffeeShopSimulator

--- src/mPatterns/decorator/DecoratorCoffeeShopSimulator.java
+++ src/mPatterns/decorator/DecoratorCoffeeShopSimulator.java
@@ -2,7 +2,7 @@ package mPatterns.decorator;
 
 public class DecoratorCoffeeShopSimulator {
     public static void main(String[] a) {
-        DecoratorBeverage myBeverage = new DecoratorWhip(new DecoratorMilk(new DecoratorEspresso()));
+        DecoratorBeverage myBeverage = new DecoratorMilk(new DecoratorWhip(new DecoratorEspresso()));
         System.out.println(myBeverage.getDescription());
         System.out.println(myBeverage.cost());
     }
