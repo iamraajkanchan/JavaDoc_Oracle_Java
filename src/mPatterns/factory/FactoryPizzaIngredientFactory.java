2367cd6cbc8ba489dd1c75a3731d66b0a8fd8312
iamraajkanchan <iamraajkanchan@gmail.com>
2023-03-01 15:11:04 +0530

    FactoryPizzaIngredientFactory interface is added in factory package inside mPatterns.

--- src/mPatterns/factory/FactoryPizzaIngredientFactory.java
+++ src/mPatterns/factory/FactoryPizzaIngredientFactory.java
@@ -1,5 +1,13 @@
 package mPatterns.factory;
 
 public interface FactoryPizzaIngredientFactory {
+    public FactoryCheese createCheese();
 
+    public FactoryClams createClams();
+
+    public FactoryDough createDough();
+
+    public FactorySauce createSauce();
+
+    public String[] createVeggies();
 }
